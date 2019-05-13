789c9b9e7eafdd43bd799b4a5dbd3505cef03932
Dorian Morones <dorian.morones@nxp.com>
2019-05-13 18:53:02 -0500

    Login fb/google and logout
    create login forms with facebook and google and Logout function

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 import FacebookLogin from 'react-facebook-login';
+import { GoogleLogin } from "react-google-login";
+
 import './index.css';
-import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/css/materialize.min.css';
+import { Redirect } from "react-router-dom";
+
 
 class Login extends Component {
   constructor() {
     super();
+    this.state = {
+      isLogged: false,
+    }
     this.responseFacebook = this.responseFacebook.bind(this);
-    this.onFailire = this.onFailire.bind(this)
+    this.responseGoogle = this.responseGoogle.bind(this);
+    this.onFailure = this.onFailure.bind(this)
   }
   responseFacebook(response) {
     console.log(response)
@@ -15,15 +23,35 @@ class Login extends Component {
       toke: response.token,
       email: response.email,
       name: response.name,
-      picture: response.picture.data.url
+      picture: response.picture.data.url,
+      social: "Facebook"
     }))
+
+    this.setState({ isLogged: true })
   }
-  onFailire(err) {
+
+  responseGoogle(response) {
+    console.log(response)
+    localStorage.setItem("GData", JSON.stringify({
+      token: response.token,
+      email: response.profileObj.email,
+      name: response.profileObj.name,
+      picture: response.profileObj.imageURL,
+      social: "Google",
+    }))
+
+    this.setState({ isLogged: true })
+  }
+
+  onFailure(err) {
     console.log(err)
   }
 
 
   render() {
+    if (this.state.isLogged) {
+      return (<Redirect to="/home/" />);
+    }
     return (
       <div className="Login">
         <div className="Login-box">
@@ -38,9 +66,15 @@ class Login extends Component {
                 textButton="Facebook"
                 cssClass="waves-effect waves-light btn blue darken-2"
                 icon="fa fa-facebook" />
-              <button className="waves-effect waves-light btn red lighten-1" id="google">Google
-              <i className="fab fa-google"></i>
-              </button>
+
+              <GoogleLogin
+                clientId="372353009990-2kvricu0jcqfkmrchfack3mmc5lv85cu.apps.googleusercontent.com"
+                autoLoad={false}
+                onSuccess={this.responseGoogle}
+                onFailure={this.onFailure}
+                className="waves-effect waves-light btn lighten-1"
+              />
+
             </div>
           </div>
         </div>
